617c921b90ed63c9a116c30b99fa5a8d917e865e
arkery <senseiark@gmail.com>
2021-05-03 21:26:16 -0700

    Redid text input for new note
    Redid text input. Now works on Android. Removed trash button from NewNote.tsx - not needed.
    
    Issue with text input being white

--- screens/NewNote.tsx
+++ screens/NewNote.tsx
@@ -6,44 +6,45 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import uuid from 'react-native-uuid';
 import Notepad from '../components/Notepad';
 import { Note } from '../types';
+import Dialog from "react-native-dialog";
 
 export default function NewNote({importData}: any) {
   const [note, setNote] = React.useState({
     title: 'Untitled Note',
     body: '',
   });
-  
+  const [visible, setVisible] = React.useState(false);
+  const [notename, setNoteName] = React.useState({name: ''}); 
+
+  const ShowDialog = () => {
+    setVisible(true)
+  }
+
+  const handleCancel = () => {
+    setVisible(false)
+  }
+
   const notepadCallback = React.useCallback((returnedNote) => {
     setNote({...note, body: returnedNote});
   }, []);
 
+  
   const resetPage = () => {
     setNote({title: 'Untitled Note', body: ''});
   };
+  
 
-  const saveNote = () => {
-    Alert.prompt("Save Note",
-    "Enter a name for your new note:",
-    [
-      {
-        text: "Cancel",
-        style: "default",
-      },
-      {
-        text: "Confirm",
-        onPress: (name) => {
-          if (name) {
-            const newNote = {title: name, body: note.body}; //Alt solution bc react hooks are async
-            storeData(newNote);
-          } else {
-            storeData(note)
-          }
-        },
-        style: "default",
-      },
-    ]);
+  const saveNotes = () => {
+    if({notename}){
+      const newNote = {title: notename.name, body: note.body};
+      storeData(newNote); 
+    } else{
+      storeData(note);
+    }
+    handleCancel(); 
   };
 
+  /*
   const deleteNote = () => {
     Alert.alert("Delete Note",
     "Press OK to delete your note",
@@ -59,7 +60,7 @@ export default function NewNote({importData}: any) {
       },
     ]);   
   };
-
+  */
   const storeData = async (value: Note) => {
     try {
       let k = String(uuid.v4())
@@ -76,19 +77,32 @@ export default function NewNote({importData}: any) {
       <View style={styles.container}>
         <Notepad userNotes={note} notepadCallback={notepadCallback} />
         
-        <Ionicons
+        {/**
+          <Ionicons
           name="trash"
           size={69}
           style={styles.deleteButton}
           onPress={deleteNote}
-        />
+          />
+         */}
 
         <Ionicons 
           name="add-circle"
           size={69}
           style={styles.saveButton}
-          onPress={saveNote}
-      />
+          onPress={ShowDialog}
+        />
+
+        <Dialog.Container visible={visible}>
+          <Dialog.Title>Add New Note</Dialog.Title>
+          <Dialog.Description>Enter a name for your new note</Dialog.Description>
+          <Dialog.Input onChangeText={(val) => setNoteName({name: val})}/>
+          <Dialog.Button label="Cancel" onPress= {handleCancel}/>
+          <Dialog.Button label="Confirm" onPress= {saveNotes}/>
+
+         </Dialog.Container>
+
+
       </View>
     </TouchableWithoutFeedback>
   );
